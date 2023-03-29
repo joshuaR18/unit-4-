1cef3b14a13bcc6cae99e446069ba65b4218412d
joshuaR18 <122958298+joshuaR18@users.noreply.github.com>
2023-03-29 15:48:14 -0500

    Update guessingGame.kt

--- guessingGame.kt
+++ guessingGame.kt
@@ -1,11 +1,11 @@
 import kotlin.random.Random
 
 fun main() {
-    val numberToGuess = Random.nextInt(1, 101) // generates a random number between 1 and 100
+    val numberToGuess = Random.nextInt(1, 101) 
     var userGuess: Int
     do {
         print("Guess the number between 1 and 100: ")
-        userGuess = readLine()?.toIntOrNull() ?: 0 // reads the user's input and converts it to an integer
+        userGuess = readLine()?.toIntOrNull() ?: 0 
         if (userGuess > numberToGuess) {
             println("Too high, try again.")
         } else if (userGuess < numberToGuess) {
